bec3898523f9196372f687ea7e41e684de441c7d
FirojBabu <firojbabu55@gmail.com>
2026-08-02 18:51:39 +0000

    fix

--- Application-Code/backend/src/app.js
+++ Application-Code/backend/src/app.js
@@ -1,6 +1,5 @@
 
 
-
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -9,35 +8,10 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-app.use((req, res, next) => {
-  console.log("CORS request origin:", req.headers.origin, "method:", req.method, "path:", req.path);
-  next();
-});
-
-const corsOptions = {
-  origin: function(origin, callback) {
-    const allowedOrigins = [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "http://localhost:5000",
-      "http://127.0.0.1:5000",
-    ];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error(`Origin ${origin} not allowed by CORS`));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-  exposedHeaders: ["Set-Cookie"],
-  credentials: true,
-  optionsSuccessStatus: 204,
-  preflightContinue: false,
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 app.use(cookieParser());
 
 app.use(express.json({
@@ -71,7 +45,7 @@ app.get("/health", (req, res) => {
     status: "ok"
   });
 });
+
 app.use(errorHandler);
 export {app};
-
 
